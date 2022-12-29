56d184bfd94419cbcd7482aa11d401c81f5f0241
khoirxz <khoirrurrohman@gmail.com>
2022-12-29 17:42:41 +0700

    padding

--- components/Atom/Section/index.jsx
+++ components/Atom/Section/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Section = ({ children }) => {
   return (
-    <section className="max-w-[800px] mx-auto px-0 md:px-6 xl:px-0 relative z-10">
+    <section className="max-w-[800px] mx-auto px-6 xl:px-0 relative z-10">
       <div className="mx-auto">{children}</div>
     </section>
   );
